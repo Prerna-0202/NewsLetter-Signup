4c7a658fcce5293046fb72422ac4f19c289ef5a9
Rahul Kumar <54908639+Purpledevil27@users.noreply.github.com>
2022-10-03 18:03:14 +0530

    Update App.js
    Hide credentials using dotenv package

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
 //jshint esversion:6
 
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const request = require('request');
-const https =require('https');
+const https = require('https');
 const { dir } = require('console');
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/",function(req,res)
-{
-    res.sendFile(__dirname+"/signup.html");
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html");
 });
-app.post("/",function(req,res)
-{
+app.post("/", function (req, res) {
     const firstName = req.body.FName;
-    const lastName =req.body.LName;
-    const email =req.body.email;
-    const data ={
-        members:[
+    const lastName = req.body.LName;
+    const email = req.body.email;
+    const data = {
+        members: [
             {
-                email_address :email,
-                status : "subscribed",
+                email_address: email,
+                status: "subscribed",
                 merge_fields: {
                     FNAME: firstName,
                     LNAME: lastName,
@@ -30,41 +29,32 @@ app.post("/",function(req,res)
             }
         ]
     };
-   const jsonData= JSON.stringify(data);
-   const url ="https://us18.api.mailchimp.com/3.0/lists/2799efa151?fields=name";
-   const options ={
-       method: "POST",
-       auth: "prerna00:b21670e66c645538b5e2ba5f97885a15-us18"
-   };
-   const request = https.request(url, options, function(response)
-   {
-       if(response.statusCode===200)
-       {
-           res.sendFile(__dirname+"/success.html");
-       }
-       else
-       {
-           res.sendFile(__dirname+"/failure.html");
-       }
-   response.on("data",function(data)
-   {
-     console.log(JSON.parse(data));
-   })
-   })
-   request.write(jsonData);
-   request.end();
+    const UNIQUE_ID = process.env.UNIQUE_ID;
+    const API_KEY = process.env.API_KEY;
+    const jsonData = JSON.stringify(data);
+    const url = `https://us18.api.mailchimp.com/3.0/lists/${UNIQUE_ID}?fields=name`;
+    const options = {
+        method: "POST",
+        auth: `prerna00: ${API_KEY}`
+    };
+    const request = https.request(url, options, function (response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
 });
-app.post("/failure",function(req,res)
-{
+app.post("/failure", function (req, res) {
     res.redirect("/");
 });
-app.listen(process.env.PORT || 3000, function()
-{
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server has been started on 3000 port");
 });
 
-// api key
-// b21670e66c645538b5e2ba5f97885a15-us18
-
-// unique Id
-// 2799efa151
